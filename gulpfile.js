6591794f298053719f225143cf20620c5c0b1f1f
chows23 <tsrn34388644@gmail.com>
2020-12-07 23:48:56 +0800

    minify, concatenate and add sourcemaps for css and js files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 const {src, dest, series, parallel} = require('gulp');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify-es').default;
+const cssnano = require('gulp-cssnano');
+const imagemin = require('gulp-imagemin');
 
 function htmlTask() {
   return src('src/*.html')
@@ -6,12 +11,20 @@ function htmlTask() {
 }
 
 function styleTask() {
-  return src('src/css/*.css')
+  return src(['src/css/normalize.css', 'src/css/style.css', 'src/css/style-1.css'])
+    .pipe(concat('all.css'))
+    .pipe(sourcemaps.init())
+    .pipe(cssnano())
+    .pipe(sourcemaps.write())
     .pipe(dest('dist/css'))
 }
 
 function scriptTask() {
-  return src('src/js/*.js')
+  return src(['src/js/courses.js', 'src/js/cart.js'])
+    .pipe(concat('main.js'))
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(dest('dist/js'))
 }
 
